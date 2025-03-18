0e521a7177e8214ecf370785a0c81388c0233fe9
Macarena Tagliana <m.macarena.t@gmail.com>
2025-03-18 12:37:35 -0300

    bug arreglado

--- questions.py
+++ questions.py
@@ -37,7 +37,13 @@ for _ in range(3):
     for intento in range(2):
         user_answer = input("Respuesta: ")
         # Se verifica si la respuesta es correcta
-        if (user_answer := int(user_answer)) not in range(1, 5):
+        # primero chequea q sea numeros
+        if not user_answer.isdigit():
+            print("Respuesta no válida")
+            exit(1)
+        user_answer = int(user_answer)
+        # chequea que este en el rango de respuestas permitidas
+        if user_answer not in range(1,5):
             print("Respuesta no válida")
             exit(1)
         else:

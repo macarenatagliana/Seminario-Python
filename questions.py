db3540406b7852cfa19517e490c091f6173a60b4
Macarena Tagliana <m.macarena.t@gmail.com>
2025-03-18 12:50:58 -0300

    contador de puntaje funcionando

--- questions.py
+++ questions.py
@@ -21,9 +21,12 @@ answers = [
     ),
     ("=", "==", "!=", "==="),
 ]
+
 # Índice de la respuesta correcta para cada pregunta, el el
 #mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+
+puntaje = 0
 # El usuario deberá contestar 3 preguntas
 for _ in range(3):
     # Se selecciona una pregunta aleatoria
@@ -51,11 +54,16 @@ for _ in range(3):
 
         if user_answer == correct_answers_index[question_index]:
             print("¡Correcto!")
+            puntaje +=1
             break
+        else: 
+            puntaje -= 0.5
     else:
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
         print(answers[question_index][correct_answers_index[question_index]])
+   
     # Se imprime un blanco al final de la pregunta
     print()
+print(f"Su puntaje es: {puntaje}")
